b04502c9c30d746e51714956fac68ec110c212c9
JorCantillano <44440679+JorCantillano@users.noreply.github.com>
2019-03-29 15:51:00 -0600

    Add files via upload

--- filosofos.c
+++ filosofos.c
@@ -3,9 +3,10 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
- 
+#include <ncurses.h>
+
 #define N 6 //Numero de filósofos
-#define VIDA 3
+
 #define IZQUIERDA(x)	((x) !=0 ? (x)-1 : (N-1)) //Calcula el filosofo de la izqda
 #define DERECHA(x)		(((x)+1)%N) //Calcula el filosofo de la dcha
 
@@ -14,13 +15,13 @@ typedef enum{
 }status;
 
 status estado[N]; //Estado en el que esta cada proceso en cada momento (pensando, hambriento o comiendo)
-sem_t mutex,s[N]; //Mutex: Solo un filosofo puede coger o soltar tenedores; s[N]: Controlamos si he podido coger tenedores o no, si no he podido, me bloqueo hasta que nos despierte el filosofo de la izqda o dcha
+sem_t mutex,s[N]; //Mutex: Solo un filosofo puede coger o soltar palillos; s[N]: Controlamos si he podido coger palillos o no, si no he podido, me bloqueo hasta que nos despierte el filosofo de la izqda o dcha
 
 void pensar(int id);
 void comer(int id);
-void coger_tenedores(int id);
+void coger_palillos(int id);
 void Comprobar(int id);
-void soltar_tenedores(int id);
+void soltar_palillos(int id);
 
 int main()
 {
@@ -29,15 +30,19 @@ int main()
 
 	int v[N]/*para decirle a cada hilo quien es*/,value,i;
 	sem_init(&mutex,0,1);
-	pthread_t filosofo[N];
-	void *Filosofo(void* );
-	
+	pthread_t filosofo[N]; //Nombre de los hilos correspondiente a los filosofos
+	pthread_t imp;        //Nombre del hilo de impresion
+	void *Filosofo(void* ); //Funcion filosofo
+	void *imprimir(void*); //Funcion de impresion
+
 	for(i=0;i<N;i++)				//NO SE PUEDEN LANZAR LOS HILOS HASTA QUE LOS SEMAFOROS NO ESTEN INICIALIZADOS
 	{
 		sem_init(&s[i],0,0);
 		estado[i]=PENSANDO;
 
 	}
+
+	pthread_create(&imp,NULL,imprimir,(void *) &v[i]);
 
 	for(i=0;i<N;i++)
 	{
@@ -52,8 +57,156 @@ int main()
 		pthread_join(filosofo[i],NULL);
 	}
 
+	pthread_join(imp,NULL);
 	return 0;
 }
+
+void *imprimir(void* i){
+
+	initscr();			//Inicio de biblioteca ncurses 		
+	cbreak();	
+	keypad(stdscr, TRUE);
+	scrollok(stdscr, TRUE);
+    nodelay(stdscr, TRUE);	
+	noecho();
+	int varmenu=1;
+	while(varmenu==1){
+
+		if (getch() =='s'){	
+			varmenu=0;
+		}
+
+		mvprintw(1,0,"Presionar s para salir");
+
+
+		//Imprimir filosofo 0
+		if (estado[0]==PENSANDO)
+		{	
+			if(estado[1]!=COMIENDO){mvprintw(4,23,"| ");}
+			mvprintw(2,30,"PENSANDO   ");
+			if(estado[5]!=COMIENDO){mvprintw(4,44,"| ");}
+		}
+
+		if (estado[0]==HAMBRIENTO)
+		{
+			mvprintw(2,30,"HAMBRIENTO");
+		}
+
+		if (estado[0]==COMIENDO)
+		{
+			mvprintw(4,23,"  ");
+			mvprintw(2,30,"COMIENDO   ");
+			mvprintw(4,44,"  ");
+		}
+
+		//Imprimir filosofo 1
+		if (estado[1]==PENSANDO)
+		{	
+			if(estado[0]!=COMIENDO)mvprintw(4,23,"| ");
+			mvprintw(6,18,"PENSANDO   ");
+			if(estado[2]!=COMIENDO)mvprintw(8,23,"| ");
+		}
+
+		if (estado[1]==HAMBRIENTO)
+		{
+			mvprintw(6,18,"HAMBRIENTO");
+		}
+
+		if (estado[1]==COMIENDO)
+		{
+			mvprintw(4,23,"  ");
+			mvprintw(6,18,"COMIENDO   ");
+			mvprintw(8,23,"  ");
+		}
+
+
+		//Imprimir filosofo 2
+		if (estado[2]==PENSANDO)
+		{	
+			if(estado[1]!=COMIENDO)mvprintw(8,23,"| ");
+			mvprintw(10,18,"PENSANDO   ");
+			if(estado[3]!=COMIENDO)mvprintw(12,23,"| ");
+		}
+
+		if (estado[2]==HAMBRIENTO)
+		{
+			mvprintw(10,18,"HAMBRIENTO");
+		}
+
+		if (estado[2]==COMIENDO)
+		{
+			mvprintw(8,23,"  ");
+			mvprintw(10,18,"COMIENDO   ");
+			mvprintw(12,23,"  ");
+		}
+
+		
+		//Imprimir filosofo 3
+		if (estado[3]==PENSANDO)
+		{	
+			if(estado[2]!=COMIENDO){mvprintw(12,23,"| ");}
+			mvprintw(14,30,"PENSANDO   ");
+			if(estado[4]!=COMIENDO){mvprintw(12,44,"| ");}
+		}
+
+		if (estado[3]==HAMBRIENTO)
+		{
+			mvprintw(14,30,"HAMBRIENTO");
+		}
+
+		if (estado[3]==COMIENDO)
+		{
+			mvprintw(12,23,"  ");
+			mvprintw(14,30,"COMIENDO   ");
+			mvprintw(12,44,"  ");
+		}
+
+		//Imprimir filosofo 4
+		if (estado[4]==PENSANDO)
+		{	
+			if(estado[3]!=COMIENDO)mvprintw(12,44,"| ");
+			mvprintw(10,44,"PENSANDO   ");
+			if(estado[5]!=COMIENDO)mvprintw(8,44,"| ");
+		}
+
+		if (estado[4]==HAMBRIENTO)
+		{
+			mvprintw(10,44,"HAMBRIENTO");
+		}
+
+		if (estado[4]==COMIENDO)
+		{
+			mvprintw(8,44,"  ");
+			mvprintw(10,44,"COMIENDO   ");
+			mvprintw(12,44,"  ");
+		}
+
+		//Imprimir filosofo 5
+		if (estado[5]==PENSANDO)
+		{	
+			if(estado[0]!=COMIENDO)mvprintw(4,44,"| ");
+			mvprintw(6,44,"PENSANDO   ");
+			if(estado[4]!=COMIENDO)mvprintw(8,44,"| ");
+		}
+
+		if (estado[5]==HAMBRIENTO)
+		{
+			mvprintw(6,44,"HAMBRIENTO");
+		}
+
+		if (estado[5]==COMIENDO)
+		{
+			mvprintw(4,44,"  ");
+			mvprintw(6,44,"COMIENDO   ");
+			mvprintw(8,44,"  ");
+		}
+
+		refresh();	
+	}
+	endwin();
+
+}
+
 
 void *Filosofo(void* i)
 {
@@ -63,25 +216,25 @@ void *Filosofo(void* i)
 	for(j=0;j<2;j++)
 	{
 		pensar(id);
-		coger_tenedores(id);
+		coger_palillos(id);
 		comer(id);
-		soltar_tenedores(id);
+		soltar_palillos(id);
 	}
 }
 
 void pensar(int id)
 {
-	printf("Filosofo %d pensando\n",id);
-	sleep (random() % 10);
+//	printf("Filosofo %d pensando\n",id);
+	sleep (random() % 15);
 }
 
 void comer(int id)
 {
-	printf("Filosofo %d comiendo\n",id);
-	sleep (random() % 10);
+//	printf("Filosofo %d comiendo\n",id);
+	sleep (random() % 15);
 }
 
-void coger_tenedores(int id)
+void coger_palillos(int id)
 {
 	sem_wait(&mutex); //Si un filosofo esta cogiendo tenedores, no podre coger y me quedare bloqueado. Si no hay nadie cogiendo tenedores, cogere yo
 	estado[id]=HAMBRIENTO;
@@ -99,7 +252,7 @@ void Comprobar(int id)
 	}
 }
 
-void soltar_tenedores(int id)
+void soltar_palillos(int id)
 {
 	sem_wait(&mutex); //Si hay alguien cogiendo tenedores, nadie mas puede coger. Si estoy cogiendo tenedores, bloqueo para que nadie mas pueda hacerlo 
 	estado[id]=PENSANDO;
